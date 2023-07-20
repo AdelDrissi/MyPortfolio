0f8a09f21b8671259f1cf8389e49df1665adeccf
Adel <leda91@hotmail.fr>
2023-07-20 22:44:14 +0200

    Navbar Component done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
 const Navbar = () => {
@@ -22,7 +24,31 @@ const Navbar = () => {
           isMenuOpen ? 'appearLink--open AppearLink' : ''
         }`}
       >
-        <ul className="anchorLinks__list"></ul>
+        <ul className="anchorLinks__list">
+          <li onClick={() => setIsMenuOpen(false)}>
+            <a href="#SectionWhoIam" className="LinkNavigation">
+              Mon Portrait
+            </a>
+          </li>
+          <li onClick={() => setIsMenuOpen(false)}>
+            <a href="#Projects" className="LinkNavigation">
+              Projets
+            </a>
+          </li>
+          <li onClick={() => setIsMenuOpen(false)}>
+            <a href="#Contact" className="LinkNavigation">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="IconResponsive">
+        <FontAwesomeIcon
+          icon={faBars}
+          className="NavbarIcons"
+          onClick={toggleMenu}
+          aria-label="menu de navigation"
+        />
       </div>
     </nav>
   );
