756f57a8666bc9cb542770b57f4aac7e71df7b7e
Drissi Adel <leda91@hotmail.fr>
2025-02-16 21:21:24 +0100

    Code Deleted

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import About from './components/About';
-import Technologies from './components/Techno';
-
 function App() {
   return (
     <div className="App">
       <Header />
       <Home />
       <About />
-      <Technologies/>
       <Projects />
       <Contact />
       <Footer />
